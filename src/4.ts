aa93a0599be45a92b28e0bf25b0bc5a6aad1f925
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-03-09 15:01:25 +0200

    the fourth task is completed

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+  private signature: number = Math.floor(Math.random() * (10 - 1) + 1);
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {
+    this.key = key;
+    this.door = false;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("The door is open");
+    } else {
+      console.log("The door is closed");
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open");
+    } else {
+      console.log("The door is closed");
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +54,3 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
-export {};
